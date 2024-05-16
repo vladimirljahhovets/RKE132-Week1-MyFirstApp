6c356916e7ff1c1a53d0dcaf5ccf4b6151e64447
Vladimir Ljahhovets <vladimir.ljahhovets@tktk.ee>
2024-05-16 23:06:51 +0300

    create new app

---
+++ MyFirtsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is a comment to my project
+//rakendus küsib kasutajalt tema nime
+// rakendus tervitab kasutajat tema nimepidi    
+Console.WriteLine("Enter your name:"); //output
+//string-sõne
+
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + "," +  userName+"!");
+Console.WriteLine($"Hello, {userName} !")
